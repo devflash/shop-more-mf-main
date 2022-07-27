2b94f5b7d561bbac090ef9d38a08d88a04d806f0
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2022-07-27 19:02:30 +0100

    preview UI and service integration

--- cartMF/src/pages/preview/index.js
+++ cartMF/src/pages/preview/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
+import { css } from '@emotion/react';
 import PreviewComp from '../../components/previewComp';
+import Layout from 'commonComponentMf/Layout';
 
-const Preview = () => <PreviewComp />;
+const layoutStyle = css`
+  min-height: calc(100vh - 62px);
+  padding-top: 30px;
+`;
+
+const Preview = ({ navigateRoute }) => (
+  <Layout layoutStyle={layoutStyle}>
+    <PreviewComp navigateRoute={navigateRoute} />
+  </Layout>
+);
 
 export default Preview;
