ce85752d4c1e72c24251f40356ed9156f85a4049
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2022-08-09 00:38:58 +0100

    prod config changes in productMf

--- productMF/webpack.prod.js
+++ productMF/webpack.prod.js
@@ -1,9 +1,43 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const Dotenv = require('dotenv-webpack');
+const path = require('path');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+const domain = process.env.PROD_DOMAIN;
 
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  plugins: [new Dotenv({ path: `./.env` })],
+  plugins: [
+    new ModuleFederationPlugin({
+      name: 'productMf',
+      filename: 'remoteEntry.js',
+      remotes: {
+        commonComponentMf: `commonComponentMf@${domain}/commonComponentMf/remoteEntry.js`,
+      },
+      exposes: {
+        './Products': './src/pages/products',
+        './Product': './src/pages/product',
+      },
+      shared: [
+        { react: { requiredVersion: '^18.1.0' } },
+        'react-dom/client',
+        {
+          '@devflash/shared-shopmore-lib': {
+            import: '@devflash/shared-shopmore-lib',
+            requiredVersion: '3.0.1',
+          },
+        },
+        'axios',
+        'react-router-dom',
+        '@emotion/react',
+      ],
+    }),
+    new Dotenv({ path: `./.env` }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+  },
 });
