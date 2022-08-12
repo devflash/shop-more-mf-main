81f7f0f2be37be750a1ae042d27828e6eb7ba609
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2022-08-13 00:28:47 +0100

    workflow in wishlistMf

--- wishlistMF/src/components/authReactContext.js
+++ wishlistMF/src/components/authReactContext.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useAuthContext } from '@devflash/shared-shopmore-lib';
 const AuthReactContext = (props) => {
+  console.log('wishlist');
   const { authUser } = useAuthContext();
 
   return <>{props.render(authUser)}</>;
