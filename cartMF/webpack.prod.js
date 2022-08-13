bc115e5a569b8b743ff39c1d9667cd8e48c74998
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2022-08-13 20:03:12 +0100

    added prod domain env in cart and comoponent mf

--- cartMF/webpack.prod.js
+++ cartMF/webpack.prod.js
@@ -41,5 +41,6 @@ module.exports = merge(common, {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
+    publicPath: '/cartmf/latest/',
   },
 });
--- commonComponentsMf/webpack.prod.js
+++ commonComponentsMf/webpack.prod.js
@@ -8,5 +8,6 @@ module.exports = merge(common, {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[contenthash].js',
+    publicPath: '/commonComponentsmf/latest/',
   },
 });
