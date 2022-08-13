c403cc1225eaa578a6e3cf259abf8579e585b69f
Mrugesh Wadagbalkar <mrugeshwadagbalkar@gmail.com>
2022-08-14 00:04:04 +0100

    env variable fix in all mf

--- wishlistMF/src/components/authReactContext.js
+++ wishlistMF/src/components/authReactContext.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useAuthContext } from '@devflash/shared-shopmore-lib';
 const AuthReactContext = (props) => {
-  console.log('wishlist');
+  console.log('wishlist!');
   const { authUser } = useAuthContext();
 
   return <>{props.render(authUser)}</>;
